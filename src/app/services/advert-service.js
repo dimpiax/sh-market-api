7c81e6662a59c1aee5cfe440cac89d69912d4ec2
Pilipenko Dima <dimpiax@gmail.com>
2017-04-02 01:29:25 +0300

    implemented adverts query

--- src/app/services/advert-service.js
+++ src/app/services/advert-service.js
@@ -7,10 +7,16 @@ import Advert from '../../database/schemas/advert'
 
 export default class AdvertService {
     static async getAdverts({ tag, toSell, interval, name }: { tag: string, toSell: boolean, interval: string, name: string }): Promise<Advert[]> {
-        let query = { tag, toSell }
+        const query = {}
+        if (tag != null) query.tag = tag
+        if (toSell != null) query.toSell = toSell
+        if (name != null) query.artName = new RegExp(`^${name}.*`)
 
-        // TODO: parse interval to min/max object format
-        // TODO: wrap name in regex
+        // mould possible range
+        if (interval != null) {
+            const rangeValue = this._getMongoRangeValue(interval)
+            if (rangeValue != null) query.price = rangeValue
+        }
 
         try {
             const result = await dbProxy.find(ModelName.advert, query)
@@ -18,5 +24,31 @@ export default class AdvertService {
         } catch (err) {
             throw err
         }
+    }
+
+    static _getMongoRangeValue(value: string): ?(number | { gte: number, lte: number }) {
+        // expression group strict input number values,
+        // searching for alone and range values
+        const regExp = /(?:^(\d+)$)|(?:(\d+)?-(\d+)?)/
+        const res = regExp.exec(value)
+        if (res == null) return null
+
+        const signleValue = res[1]
+        if (signleValue != null) {
+            // in case input signle number, i.e.: 123
+            return parseInt(signleValue, 10)
+        }
+
+        // in case input range value, i.e.: 1-, -1, 1-2
+        const range = { $gte: res[2], $lte: res[3] }
+        const result = Object.keys(range)
+            .map((el: string): ?Object => {
+                const number = range[el]
+                if (number == null) return null
+                return { [el]: parseInt(number, 10) }
+            })
+            .filter(Boolean)
+
+        return result.length > 0 ? Object.assign({}, ...result) : null
     }
 }
